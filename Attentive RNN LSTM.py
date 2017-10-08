3106996d2bc4b51ad4c3e06dad19b5c6a8610510
Sagar Jaiswal <jas_96@hotmail.co.uk>
2017-10-08 22:43:57 +0100

    Answers and further code added
    Arny will be able to perform several actions listed in the answer text file

--- Attentive RNN LSTM.py
+++ Attentive RNN LSTM.py
@@ -1,3 +1,19 @@
-from keras.models import Sequential, Model
+import numpy as np
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, TimeDistributed, Bidirectional, Input
+from keras.layers import LSTM
+from keras.utils import np_utils
 
+
+
+output_dim
+output_length
+batch_input_shape=None,
+batch_size=None
+input_shape=None
+input_length=None
+input_dim=None
+hidden_dim=256
+depth=1
+bidirectional=True
+unroll=False
